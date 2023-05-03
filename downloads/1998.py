0e4becf9c7ee0589695be3cf520b4c91c5d92c95
41023125 <41023125@gm.nfu.edu.tw>
2023-05-03 16:42:26 +0800

    fotball2-連線加計分板.ttt lua all

--- downloads/1998.py
+++ downloads/1998.py
@@ -5,7 +5,7 @@ import keyboard
 
 # Connect to the simulation
 sim.simxFinish(-1)
-clientID = sim.simxStart('192.168.56.1', 19997, True, True, 5000, 5)
+clientID = sim.simxStart('127.0.0.1', 19997, True, True, 5000, 5)
 
 if clientID != -1:
     print('Hello,bubbleRob,我們是ag14分組')
@@ -14,9 +14,9 @@ if clientID != -1:
     res, bubbleRob = sim.simxGetObjectHandle(clientID, 'bubbleRob', sim.simx_opmode_oneshot_wait)
     res, leftMotor = sim.simxGetObjectHandle(clientID, 'leftMotor', sim.simx_opmode_oneshot_wait)
     res, rightMotor = sim.simxGetObjectHandle(clientID, 'rightMotor', sim.simx_opmode_oneshot_wait)
-    leftMotor = 28
-    rightMotor = 26
-    bubbleRob = 23
+    #leftMotor = 28
+    #rightMotor = 26
+    #bubbleRob = 16
     # Set the initial motor velocities to zero
     sim.simxSetJointTargetVelocity(clientID, leftMotor, 0, sim.simx_opmode_oneshot_wait)
     sim.simxSetJointTargetVelocity(clientID, rightMotor, 0, sim.simx_opmode_oneshot_wait)
